4c2d2c308b23893908a4ccb2f440775b1d4d070b
Shejel <ArteKirill2006@gmail.com>
2025-04-07 15:32:10 +0300

    code

---
+++ Artemenko_project_LR4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void prov(int& number1)
+{
+	
+}
+
+void prov(float& number1)
+{
+	
+}
+
+void task1(float num1)
+{
+    
+}
+
+void task2(float num2)
+{
+    
+}
+
+void task3(float num1, float num2)
+{
+    
+}
+
+void task4(float num1,float num2)
+{
+    
+}
+
+int main()
+{
+        
+}
