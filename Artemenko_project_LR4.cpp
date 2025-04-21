54642060ffe727577273ef9be0022a6ba51e4f1a
Shejel <ArteKirill2006@gmail.com>
2025-04-21 11:11:03 +0300

    task3

--- Artemenko_project_LR4.cpp
+++ Artemenko_project_LR4.cpp
@@ -23,7 +23,7 @@ void task2(float num2)
 
 void task3(float num1, float num2)
 {
-    
+    cout<<"Сумма равна "<<num1 + num2<<endl;
 }
 
 void task4(float num1,float num2)
@@ -33,5 +33,26 @@ void task4(float num1,float num2)
 
 int main()
 {
-        
+    setlocale(LC_ALL,"rus");
+    int answer;
+    float num1;
+    float num2;
+    prov(answer);
+    switch (answer)
+    {
+        case 1:
+            task1(num1);
+            main();
+        case 2:
+            task2(num2);
+            main();
+        case 3:
+            task3(num1, num2);
+            main();
+        case 4:
+            task4(num1, num2);
+            main();
+        default:
+            cout<<"Введите корректное число: ";
+            main();    
 }
